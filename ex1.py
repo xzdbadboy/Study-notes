f1e88ca47124d2ab916da17e746f7195ce05ffb8
xzdbadboy <audie@vip.qq.com>
2023-04-19 10:52:15 +0800

    学习记录上传

--- ex1.py
+++ ex1.py
@@ -1,8 +1,8 @@
 print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "Yay!Printing."
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
-print '测试下'
-print "Pycharm 添加文件"
+# print "Hello Again"
+# print "I like typing this."
+# print "Yay!Printing."
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+# print '测试下'
+# print "Pycharm 添加文件"
